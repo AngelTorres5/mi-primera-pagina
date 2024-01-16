c67f16383331fa971500d8540e4c7c27ee8c9f1d
angel torres <at77131@gmail.com>
2024-01-16 16:07:40 -0600

    mi primer commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function nuevaVentana() {
+    var URL = "https://www.amazon.com"; // Reemplaza esto con la URL de la página a la que deseas dirigir
+    window.open(URL, '_blank');
+}
+
+function nuevaVentana2() {
+    var URL = "https://www.pedidosonline.com"; // Reemplaza esto con la URL de la página a la que deseas dirigir
+    window.open(URL, '_blank');
+}
+
+function nuevaVentana3() {
+    var URL = "https://www.glovoapp.com"; // Reemplaza esto con la URL de la página a la que deseas dirigir
+    window.open(URL, '_blank');
+}
